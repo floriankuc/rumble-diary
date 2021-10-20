181af917fc274ac3b0dd63d24bc31e652d803b5f
Florian Kuc <florian.kuc@gmail.com>
2021-10-20 10:14:17 +0200

    refactor: login to class

--- client/src/containers/Login.tsx
+++ client/src/containers/Login.tsx
@@ -1,51 +1,52 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { useHistory } from 'react-router';
+import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 import { login } from '../actions/authActions';
 import { clearErrors } from '../actions/errorActions';
 import Login from '../components/Login';
 import { APP_ROUTES } from '../routes';
+import history from '../routes/history';
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
 
 export interface LoginModalProps {
-  isAuthenticated: boolean;
-  error: any;
-  login: ({ email, password }: { email: any; password: any }) => (dispatch: any) => Promise<void>;
+  // login: ({ email, password }: { email: any; password: any }) => (dispatch: Promise<void>) => Promise<void>;
   clearErrors: () => { type: string };
 }
 
-const LoginModal = ({ isAuthenticated, error, login, clearErrors }: any) => {
-  const history = useHistory();
-  const [msg, setMsg] = useState(null);
+export interface LoginReduxProps extends PropsFromRedux {
+  isAuthenticated: boolean;
+  error: any;
+}
 
-  const handleSubmit = useCallback(
-    (values) => {
-      login({ email: values.email, password: values.password });
+class LoginModal extends React.Component<LoginModalProps & LoginReduxProps> {
+  state = { msg: null };
 
-      clearErrors();
-    },
-    [clearErrors, login]
-  );
+  handleSubmit = (values: any) => {
+    this.props.login({ email: values.email, password: values.password });
+    clearErrors();
+  };
 
-  useEffect(() => {
-    if (isAuthenticated) {
+  componentDidMount() {
+    if (this.props.isAuthenticated) {
       history.push(APP_ROUTES.start);
     }
-  }, [isAuthenticated, history]);
-
-  useEffect(() => {
-    if (error.id === 'LOGIN_FAIL') {
-      setMsg(error.msg.msg);
+    if (this.props.error.id === 'LOGIN_FAIL') {
+      this.setState({ msg: this.props.error.msg.msg });
     } else {
-      setMsg(null);
+      this.setState({ msg: null });
     }
-  }, [error, isAuthenticated]);
+  }
 
-  return <Login msg={msg} handleSubmit={handleSubmit} />;
-};
+  render() {
+    return <Login msg={this.state.msg} handleSubmit={this.handleSubmit} />;
+  }
+}
 
 const mapStateToProps = (state: any) => ({
   isAuthenticated: state.auth.isAuthenticated,
   error: state.error,
 });
 
-export default connect(mapStateToProps, { login, clearErrors })(LoginModal);
+const connector = connect(mapStateToProps, { login, clearErrors });
+
+export default connector(LoginModal);
