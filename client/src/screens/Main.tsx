c0cd016464f6530f9c076ce497475db169607967
Florian Kuc <florian.kuc@gmail.com>
2021-11-01 19:59:06 +0100

    feat: 404

--- client/src/screens/Main.tsx
+++ client/src/screens/Main.tsx
@@ -6,14 +6,13 @@ import List from '../containers/List';
 import Modal from '../components/Modal/Modal';
 import LoginModal from '../containers/Login';
 import { APP_ROUTES } from '../routes';
-// import { Sidebar } from '../components/Sidebar/Sidebar';
 import SidebarContainer from '../containers/Sidebar';
 import { connect, ConnectedProps } from 'react-redux';
 import RegisterModal from '../containers/Register';
 import Show from '../containers/EditForm';
 import Root from '../components/Root';
 import { AppState } from '../reducers';
-// import { NotFound404 } from '../components/404';
+import { NotFound404 } from '../components/404';
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
@@ -27,20 +26,21 @@ const MainScreen = (props: IMain) => {
   const renderRegisterModal = (): ReactElement => <Modal component={<RegisterModal />} />;
   const location = useLocation();
 
+  const match404 = location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/register';
+  const matchAuth = !props.isLoading && !props.isAuthenticated && match404;
+
   return (
     <div>
       <AppNavbar />
       <SidebarContainer />
       <div style={{ display: 'flex', flexDirection: 'column', padding: '80px 0px', alignItems: 'center' }}>
         <Switch>
-          {!props.isLoading && !props.isAuthenticated && location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/register' && (
-            <Redirect to="/" />
-          )}
-          <Route path={APP_ROUTES.start} exact component={(): ReactElement => <div>start</div>} />
+          {matchAuth && <Redirect to="/" />}
           <Route path={APP_ROUTES.add} exact component={ItemModal} />
           <Route path={APP_ROUTES.diary} exact component={List} />
           <Route path={APP_ROUTES.show} exact component={Show} />
-          <Route path={APP_ROUTES.root} component={Root} />
+          <Route path={APP_ROUTES.root} exact={match404} component={Root} />
+          {match404 && <Route component={NotFound404} />}
         </Switch>
       </div>
       <Switch>
